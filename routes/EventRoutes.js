31cfa9b8600bc44b9c932208d3f46864ce804bcd
Sathsara98 <sathsarat98@gmail.com>
2026-04-25 10:14:37 +0530

    feat: implement SendGrid email notifications for event registration and password reset

--- routes/EventRoutes.js
+++ routes/EventRoutes.js
@@ -2,35 +2,90 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const Event = require("../schemas/Event");
-const auth = require("../Authentication/Auth");
+const auth = require("../authentication/Auth");
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const SENDGRID_ENDPOINT = "https://api.sendgrid.com/v3/mail/send";
+const SENDGRID_FROM_EMAIL = process.env.FROM_EMAIL;
+const SENDGRID_FROM_NAME = "SmartMeet";
 
-router.post("/new", function (req, res, next) {
-  Event.create({
-    sector: req.body.sector,
-    name: req.body.name,
-    venue: req.body.venue,
-    location: req.body.location,
-    time: req.body.time,
-    members: req.body.members,
-    date: req.body.date,
-    questions: req.body.questions,
-  })
-    .then(function (item) {
-      req.body.members.map((i) => {
-        sendEventNotification(
-          i.name,
-          i.email,
-          req.body.date,
-          req.body.time,
-          req.body.venue,
-          req.body.location,
-          req.body.name
-        );
+function buildEmailHtml(data) {
+  return data
+    .map((item) => `<p><strong>${item.field || ""}</strong> ${item.value || ""}</p>`)
+    .join("");
+}
+
+function sendEmailViaSendGrid(receiver, subject, data) {
+  if (!SENDGRID_API_KEY || !SENDGRID_FROM_EMAIL) {
+    console.error("Missing SendGrid configuration. Skipping email send.");
+    return Promise.resolve();
+  }
+
+  return axios.post(
+    SENDGRID_ENDPOINT,
+    {
+      personalizations: [
+        {
+          to: [{ email: receiver }],
+          subject,
+        },
+      ],
+      from: { email: SENDGRID_FROM_EMAIL, name: SENDGRID_FROM_NAME },
+      content: [
+        {
+          type: "text/html",
+          value: buildEmailHtml(data),
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${SENDGRID_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
+
+
+router.post("/new", async function (req, res) {
+  try {
+    const newEvent = await Event.create({
+      sector: req.body.sector,
+      name: req.body.name,
+      venue: req.body.venue,
+      location: req.body.location,
+      time: req.body.time,
+      members: Array.isArray(req.body.members) ? req.body.members : [],
+      date: req.body.date,
+      questions: Array.isArray(req.body.questions) ? req.body.questions : [],
+    });
+
+    if (Array.isArray(req.body.members)) {
+      req.body.members.forEach((i) => {
+        if (i.email) {
+          sendEventNotification(
+            i.name,
+            i.email,
+            req.body.date,
+            req.body.time,
+            req.body.venue,
+            req.body.location,
+            req.body.name
+          );
+        }
       });
-      res.send(item);
-    })
-    .catch(next);
+    }
+
+    return res.status(201).json(newEvent);
+  } catch (error) {
+    console.error("Error creating event:", error);
+    return res.status(500).json({
+      error: error.message || "Failed to create event",
+    });
+  }
 });
+
+
 
 router.get("/all", function (req, res, next) {
   Event.find({}, {}, { sort: { _id: -1 } }).then(function (item) {
@@ -48,46 +103,38 @@ function sendEventNotification(
   meeting
 ) {
   console.log("send email" + email);
-  axios
-    .post("https://form-to-email-api.herokuapp.com/api/email", {
-      name: "Trade Ministry Sri Lanka",
-      receiver: email,
-      subject: "You have an Upcoming Meeting",
-      data: [
-        {
-          field: "Dear " + userName + " ,",
-          value:
-            "You Have been assigned to a meeting, Meeting Details are following",
-        },
-        {
-          field: "Meeting Topic : " + meeting,
-          value:
-            "Date : " +
-            date +
-            "<br/>" +
-            "Time : " +
-            time +
-            "<br/>" +
-            "Venue : " +
-            location,
-        },
+  sendEmailViaSendGrid(email, "You have an Upcoming Meeting", [
+    {
+      field: "Dear " + userName + " ,",
+      value: "You Have been assigned to a meeting, Meeting Details are following",
+    },
+    {
+      field: "Meeting Topic : " + meeting,
+      value:
+        "Date : " +
+        date +
+        "<br/>" +
+        "Time : " +
+        time +
+        "<br/>" +
+        "Venue : " +
+        location,
+    },
 
-        {
-          field: "Navigate With Google Maps ",
-          value: '<a href="' + locationUrl + '"> Click Here </a>',
-        },
-        {
-          field: "Thank You!",
-          value: "This is System Generated Email Please Don't Reply!",
-        },
-      ],
-    })
-    .then((res) => {
-      console.log(`statusCode: ${res.statusCode}`);
-      // console.log(res);
+    {
+      field: "Navigate With Google Maps ",
+      value: '<a href="' + locationUrl + '"> Click Here </a>',
+    },
+    {
+      field: "Thank You!",
+      value: "This is System Generated Email Please Don't Reply!",
+    },
+  ])
+    .then((response) => {
+      console.log(`SendGrid status: ${response.status}`);
     })
     .catch((error) => {
-      console.error(error);
+      console.error(error.response?.data || error.message);
     });
 }
 
